6834910ed36618c23e7e2ccdeb4f564a702054dc
Valmiki yadav <valmikiyadav.bbdu@gmail.com>
2025-12-13 05:12:26 +0000

    add datatypes.js file

---
+++ jschaicode/datatypes.js
@@ -0,0 +1,8 @@
+const name="rajesh";
+let age="30";
+company="OpenAI";
+let intage=Number(age);
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(intage));
+console.log(`${typeof(company)}_${company} is working with age_${intage}`);

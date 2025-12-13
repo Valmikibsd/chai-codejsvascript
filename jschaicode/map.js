215d85d40302a2270a05ff763ff098a2a27f977a
Valmiki yadav <valmikiyadav.bbdu@gmail.com>
2025-12-13 09:25:25 +0000

    add map.js and update variable.js

---
+++ jschaicode/map.js
@@ -0,0 +1,69 @@
+const numbers = [1, 2, 3, 4];
+const aquare=numbers.map(n=>n*n);
+console.log(aquare);
+const fnumbers=numbers.filter(n=>n*n>10);
+console.log(fnumbers);
+console.log(numbers);
+
+//numbers.forEach(n=>console.log(n);
+const users = [
+    { id: 1, name: "Amit" },
+    { id: 2, name: "Vamiki" }
+];
+
+const names=users.map(u=> u.name.toUpperCase());
+console.log(names);
+
+const products = [
+    { name: "Laptop", price: 50000 },
+    { name: "Phone", price: 20000 }
+];
+
+const updatedProducts=products.map((p,index)=>({
+    ...p,
+    gstprice:p.price*2,
+    id:index+1
+}));
+
+console.log(updatedProducts);
+
+const days = ["Mon", "Tue", "Wed"];
+
+days.map((day,index)=>{
+    console.log(`${index+1} - ${day}`);
+});
+
+
+const employees = [
+  { id: 1, name: "Amittt", salary: 30000 },
+  { id: 2, name: "Vamiki", salary: 40000 }
+];
+
+const uname=employees.map(u=>u.name);
+
+console.log(uname);
+
+const employeessal = [
+  { name: "Amit", salary: 30000 },
+  { name: "Vamiki", salary: 40000 }
+];
+
+const yempsal=employeessal.map((e,i)=>({
+    ...e,
+    ctc:e.salary*12
+
+}));
+
+console.log(yempsal);
+
+
+const apiData = [
+  { id: 1, first_name: "Vamiki", last_name: "Yadav" },
+  { id: 2, first_name: "Amit", last_name: "Sharma" }
+];
+
+const fullnames=apiData.map(u=>({
+    ...u,
+    fullname:`${u.first_name} ${u.last_name}`
+}));
+console.log(fullnames);
--- jschaicode/variable.js
+++ jschaicode/variable.js
@@ -9,6 +9,13 @@ function greet() {
     const greeting = "Welcome to jschaicode!";
     var vargreeting = "This will cause an error"; // Redeclaring 'greeting' with 'var' in the same scope
     console.log(greeting);
+    console.log(vargreeting);
+}
+
+const grrting =()=>{
+    let msg = "Arrow function greeting!";
+    console.log(msg);
 }
 greet();
+grrting();
 //console.log(vargreeting);

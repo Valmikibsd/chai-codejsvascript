6774a7da3fb023d7cfcc032b3ad67d3687a80676
Valmiki yadav <valmikiyadav.bbdu@gmail.com>
2025-12-24 05:53:34 +0000

    modified:   jschaicode/objects.js

--- jschaicode/objects.js
+++ jschaicode/objects.js
@@ -20,3 +20,42 @@ myuse.getDetails=function(){
 console.log(myuse.getDetails());
 
 
+
+const tinderUser={
+    name:"aj k",
+    age:29,
+    isLoggedIn:true,
+    fullname:{
+        name:"aj",
+        lastname:"k"
+    }
+    
+};
+
+console.log("NAme:",tinderUser.fullname.name);
+
+
+const obj1={name:"aj k", age:29};
+const obj2={class:"raj", roll:230};
+
+//const objnew3=Object.assign({},obj1,obj2);
+
+const objnew3={...obj1,...obj2};
+console.log("obj:",objnew3);
+
+const keys=Object.keys(objnew3);
+const values=Object.values(objnew3);
+const entries=Object.entries(objnew3);
+
+console.log("Keys:",keys);
+console.log("Values:",values);
+console.log("Entries:",entries);
+
+const course={
+    title:"js course",
+    price:999,
+    instructor:"aj k"
+};
+
+const {instructor:ins}=course;
+console.log("Instructor:",ins);
